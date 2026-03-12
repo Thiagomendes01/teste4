a0188ea74f79113a2ff37462e1ff19d5be2f3435
Thiago Mendes <thinena02@gmail.com>
2026-03-12 17:56:02 -0300

    teste

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 30;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-function exibirTextoNaTela(tag, texto) {
+function exibirTextoNaTela(tag, texto) {    
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
